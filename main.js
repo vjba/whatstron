eb8826d296c0f8a6f30df08d4c56cd01a0d77721
vjba <vjba@pm.me>
2019-07-10 01:30:38 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,118 @@
+'use strict'
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, Menu, Tray } = require('electron')
+const { openUrlMenuItem } = require('electron-util')
+const { platform } = require('os')
+const path = require('path')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+let trayIcon = null
+let appIcon = null
+let URL = 'https://web.whatsapp.com'
+
+// Determine appropriate icon for platform
+if (platform == 'darwin' || platform == 'linux') {
+  trayIcon = path.join(__dirname, 'assets', 'icon.png')
+} else if (platform == 'win32') {
+  trayIcon = path.join(__dirname, 'assets', 'icon.ico')
+}
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    show: false,
+    title: 'WhatsTron',
+    icon: trayIcon,
+    width: 800,
+    height: 600,
+    center: true,
+    darkTheme: true,
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      nodeIntegrationInWorker: false
+    }
+  })
+
+  // Prevent HTTP document title from changing the window title
+  mainWindow.on('page-title-updated', (event) => {
+    event.preventDefault()
+  })
+
+  // Change User-Agent to circumvent 'WhatsApp works with Google Chrome 49+' alert on startup
+  mainWindow.webContents.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36')
+
+  // Click close hides window
+  mainWindow.on('close', (event) => {
+    if (mainWindow.isVisible()) {
+      event.preventDefault()
+      mainWindow.hide()
+    }
+  })
+
+  // Create tray icon
+  appIcon = new Tray(trayIcon)
+
+  // Set title for tray icon
+  appIcon.setTitle('WhatsTron')
+
+  // Set tool tip for tray icon
+  appIcon.setToolTip('WhatsTron')
+
+  // Create context menu for tray icon
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Restore Window',
+      click: () => {
+        mainWindow.show()
+      }
+    },
+    openUrlMenuItem({
+      label: 'Visit GitHub Repository',
+      url: 'https://github.com/vjba/whatstron'
+    }),
+    {
+      label: '',
+      type: 'separator'
+    },
+    {
+      label: 'Exit',
+      click: () => {
+        process.exit(0)
+      }
+    }
+  ])
+
+  // Load url
+  mainWindow.loadURL(URL)
+
+  // Create trayIcon context menu
+  appIcon.setContextMenu(contextMenu)
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+
+  // Don't show until we are ready and loaded
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  })
+}
+
+// Prevent multiple instances of the app
+app.on('second-instance', () => {
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) {
+      mainWindow.restore()
+    }
+    mainWindow.show()
+  }
+})
+
+// This method will be called when Electron has finished
+// initialization (all of the above) and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
