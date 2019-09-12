5e6d10efb26009ce1a3aaa55b8d077ca847b5a54
vjba <hello@vjba.me>
2019-09-12 17:04:29 +0100

    fix menu interaction errors

--- main.js
+++ main.js
@@ -1,14 +1,101 @@
 'use strict'
-const { app, BrowserWindow, Tray } = require('electron')
-const fetchRemoteVersion = require('./update')
+const { app, BrowserWindow, Tray, Menu, shell } = require('electron')
+const { openUrlMenuItem } = require('electron-util')
+const path = require('path')
 const URL = require('url').URL
-const { contextMenu, trayIcon } = require('./tray')
+const { fetchRemoteVersion, getLocalVersion } = require('./update')
 
 // Some global vars
 let mainWindow = null
 let appTray = null
 const appURL = 'https://web.whatsapp.com'
 const instanceLock = app.requestSingleInstanceLock()
+
+// Icon resources
+const trayIcon = path.join(__dirname, 'assets', 'icon.png')
+const exitIcon = path.join(__dirname, 'assets', 'power_settings_new.png')
+const helpIcon = path.join(__dirname, 'assets', 'help.png')
+const newIssueIcon = path.join(__dirname, 'assets', 'add_alert.png')
+const issuesIcon = path.join(__dirname, 'assets', 'bug_report.png')
+const deleteDataIcon = path.join(__dirname, 'assets', 'delete.png')
+const restoreIcon = path.join(__dirname, 'assets', 'desktop_windows.png')
+const websiteIcon = path.join(__dirname, 'assets', 'link.png')
+const restartIcon = path.join(__dirname, 'assets', 'refresh.png')
+
+const contextMenu = Menu.buildFromTemplate([
+  {
+    label: 'Restore Window',
+    icon: restoreIcon,
+    click: () => {
+      mainWindow.show()
+    }
+  },
+  openUrlMenuItem({
+    label: 'Visit Website',
+    icon: websiteIcon,
+    url: 'https://github.com/vjba/whatstron'
+  }),
+  {
+    label: '',
+    type: 'separator'
+  },
+  {
+    label: 'Help',
+    icon: helpIcon,
+    submenu: [
+      openUrlMenuItem({
+        label: 'Report Issue',
+        icon: newIssueIcon,
+        url: 'https://github.com/vjba/whatstron/issues/new/choose'
+      }),
+      openUrlMenuItem({
+        label: 'View Issues',
+        icon: issuesIcon,
+        url: 'https://github.com/vjba/whatstron/issues'
+      }),
+      {
+        label: '',
+        type: 'separator'
+      },
+      {
+        label: 'Delete App Data',
+        icon: deleteDataIcon,
+        click () {
+          shell.moveItemToTrash(app.getPath('userData'))
+          app.relaunch()
+          process.exit(0)
+        }
+      },
+      {
+        label: 'Restart App',
+        icon: restartIcon,
+        click () {
+          app.relaunch()
+          process.exit(0)
+        }
+      },
+      {
+        label: '',
+        type: 'separator'
+      },
+      {
+        label: 'WhatsTron ' + getLocalVersion(),
+        enabled: false
+      }
+    ]
+  },
+  {
+    label: '',
+    type: 'separator'
+  },
+  {
+    label: 'Exit',
+    icon: exitIcon,
+    click: () => {
+      process.exit(0)
+    }
+  }
+])
 
 // Create the browser window.
 function createWindow () {
@@ -91,11 +178,4 @@ app.on('web-contents-created', (event, contents) => {
   })
 })
 
-app.on('ready', createWindow)
-
-// Check for update
-app.on('ready', fetchRemoteVersion)
-
-module.exports.app = app
-module.exports.BrowserWindow = BrowserWindow
-module.exports.mainWindow = mainWindow
+app.on('ready', createWindow, fetchRemoteVersion)
